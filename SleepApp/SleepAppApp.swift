46a958dd78b61a3619dfe8d527380b9b655f297a
Marko <markosentivanac1@gmail.com>
2024-06-26 14:18:53 +0200

    Initial Commit

---
+++ SleepApp/SleepAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepAppApp.swift
+//  SleepApp
+//
+//  Created by Marko Sentivanac on 26.6.24..
+//
+
+import SwiftUI
+
+@main
+struct SleepAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
